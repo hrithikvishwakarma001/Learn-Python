f20f6e7c8715747c517f02cc91550b188df9fefc
Hrithik Vishwakarma <hritik8008@gmail.com>
2023-08-09 01:01:40 +0530

    ready for production

--- django/crudApp/crudApp/settings.py
+++ django/crudApp/crudApp/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-y_uj=o%_*qy+agc9$&sniws(e7wy$237(4m(v*6nkn4emtq#mv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -128,3 +128,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+# deployment
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
