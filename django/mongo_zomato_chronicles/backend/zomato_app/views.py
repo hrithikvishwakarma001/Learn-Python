c80c337f22d1e3b7abd200890f05d2bce21f0eb1
Hrithik Vishwakarma <hritik8008@gmail.com>
2023-08-11 21:54:56 +0530

    all functin=onality is done

--- django/mongo_zomato_chronicles/backend/zomato_app/views.py
+++ django/mongo_zomato_chronicles/backend/zomato_app/views.py
@@ -94,3 +94,51 @@ def create_orders_item_api(request):
     except Exception as e:
         return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
+
+@api_view(["GET"])
+def get_orders_items_api(request):
+    try:
+        orders_collection = db["orders"]
+        orders_items = orders_collection.find()
+        serialized_orders_items = [
+            {
+                "id": str(item["_id"]),
+                "customer_name": item["customer_name"],
+                "dishes_ids": item["dishes_ids"],
+                "total_price": item["total_price"],
+                "status": item["status"],
+                "order_date": item["order_date"],
+            }
+            for item in orders_items
+        ]
+        return Response({"orders_items": serialized_orders_items, "success": True})
+    except Exception as e:
+        return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+@api_view(["PUT"])
+def update_orders_status_item_api(request, item_id):
+    try:
+        data = request.data
+        orders_collection = db["orders"]
+        updated_item = {
+            "status": data["status"],
+        }
+        orders_collection.update_one({"_id": ObjectId(item_id)}, {"$set": updated_item})
+        return Response(
+            {"message": "orders item updated successfully", "success": True}
+        )
+    except Exception as e:
+        return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+@api_view(["DELETE"])
+def delete_orders_item_api(request, item_id):
+    try:
+        orders_collection = db["orders"]
+        orders_collection.delete_one({"_id": ObjectId(item_id)})
+        return Response(
+            {"message": "orders item deleted successfully", "success": True}
+        )
+    except Exception as e:
+        return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
